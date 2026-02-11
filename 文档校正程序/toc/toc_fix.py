5255ef92b185445d3eb58f729330b9c569224430
wliwh <wanghn0409@gmail.com>
2026-02-11 15:30:42 +0800

    更新

--- 文档校正程序/toc/toc_fix.py
+++ 文档校正程序/toc/toc_fix.py
@@ -8,9 +8,9 @@ def toc_file_corr(file_pth:str, add_num:int = 0):
     for l in lines:
         l = l.strip()
         # lr = re.match(r'^(\d+)\s+[\(\)\-p\d:]+\s+(.+)',l)
-        lr = re.match(r'[\(\)\.p\d]+\s+\(p(\d+)\)\s+(.+)',l)
+        lr = re.match(r'[\(\)\-\.p\d]+\s+\(p(\d+)\):\s+(.+)',l)
         if lr:
-            num = l.count(r'.')
+            num = l.count(r'-')
             lr = lr.groups()
             nline = ' '*(4*num) + lr[1] + f' {(int(lr[0])+add_num)}'
             toc_lst.append(nline)
@@ -23,4 +23,4 @@ def toc_file_corr(file_pth:str, add_num:int = 0):
 
 if __name__ == '__main__':
     # process_file(r'/home/hh01/Downloads/works/文档校正/hrlA.md', 220)
-    toc_file_corr(r"/home/hh01/Downloads/L/1引言.txt", 1)
+    toc_file_corr(r"C:\Users\84066\Downloads\1.txt", 11)
